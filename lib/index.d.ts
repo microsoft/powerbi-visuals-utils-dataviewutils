72319375cd46b84a98ce81ad7681bb08098cadfd
Ilfat Galiev <v-ilgali@microsoft.com>
2018-10-24 15:34:59 +0300

    Update packages to fix vulnerabilities
    API 2.1.0 support

--- lib/index.d.ts
+++
@@ -1,8 +0,0 @@
-import * as converterHelper from "./converterHelper";
-import * as dataRoleHelper from "./dataRoleHelper";
-import * as dataViewObject from "./dataViewObject";
-import * as dataViewObjects from "./dataViewObjects";
-import * as dataViewObjectsParser from "./dataViewObjectsParser";
-import * as dataViewTransform from "./dataViewTransform";
-import * as validationHelper from "./validationHelper";
-export { converterHelper, dataRoleHelper, dataViewObject, dataViewObjects, dataViewObjectsParser, dataViewTransform, validationHelper };
--- lib/validationHelper.d.ts
+++
@@ -1,4 +0,0 @@
-export declare module validationHelper {
-    function isImageUrlAllowed(url: string): boolean;
-    function isFileImage(url: string, imageCheckResultCallBack: (isImage: boolean, contentType: string) => void): void;
-}
