99947dee00bb905fc9c60a93d311f3613c2fae42
Alexey <v-asemenov@microsoft.com>
2022-05-24 15:01:31 +0200

    update karma config

--- karma.conf.ts
+++ karma.conf.ts
@@ -39,7 +39,7 @@ module.exports = (config) => {
     config.set({
         browsers: ["ChromeHeadless"],
         colors: true,
-        frameworks: ["jasmine", "webpack"],
+        frameworks: ["jasmine"],
         reporters: [
             "progress",
             "coverage-istanbul"
